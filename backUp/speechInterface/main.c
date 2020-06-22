9efa76d6476a030f2c348c90a3120d6b1e9648fa
bariatul <atulbari@outlook.com>
2020-06-22 03:27:49 +0000

    commit

---
+++ backUp/speechInterface/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "speechInterface.h"
+int main() {
+
+	cppwrapperFuc("test","speech.samespace.com:8080");
+	
+}
---
+++ backUp/test/aaa.h
@@ -0,0 +1,9 @@
+#ifndef AAA_H
+#define AAA_H
+
+class AAA {
+    public:
+        AAA();
+        void sayHi(const char *name);
+};
+#endif
